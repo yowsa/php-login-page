7e649e51e3a255133b08aac7654cb0831fa20eff
yowsa <josefin@fundin.com>
2019-09-27 20:17:34 +0100

    template files, main header.php

---
+++ resources/templates/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  	<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+	<script type="text/javascript" src="js/login.js"></script>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <title></title>
+
+
+  </head>
+  <body>
