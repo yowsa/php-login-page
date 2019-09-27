d151f9aa54ee2a0c3bffe35f5344939ee4f35135
yowsa <josefin@fundin.com>
2019-09-27 12:45:35 +0100

    first version of the form sending data to the database

---
+++ public_html/create_user.php
@@ -0,0 +1,35 @@
+
+<head>
+	<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+	<script type="text/javascript" src="js/login.js"></script>
+
+
+</head>
+
+<?php 
+
+echo "lets create a user shall we" ?>
+
+<div> 
+	<form id="create_user_form" method="post"> 
+		Email: <input type="name" name="email"><p>
+		Password: <input type="password" name="password"><p>
+		Confirm Password: <input type="password" name="confirm_password"><p>
+		<input type="submit" id="create_user_button" name="log in">
+	</form>
+</div>
+
+<a href="login.php">Login</a>
+
+
+
+<?php
+require "../db_connection.php";
+// TODO: check so that the passwords provided are the same
+
+if ($_POST){
+	$accounts_table->addUser($_POST["email"], $_POST["password"]);
+}
+
+
+?>
