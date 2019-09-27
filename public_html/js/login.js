b3f919a872fec86347a9343bb44f7c596fad6c25
yowsa <josefin@fundin.com>
2019-09-27 20:13:32 +0100

    removed dead code, added a few todos, added if statement to check if data from back end came bas as successful if so redirect to the logged_in page

--- public_html/js/login.js
+++ public_html/js/login.js
@@ -1,12 +1,28 @@
 $(function(){
 
 
-
-
 $("#create_user_button").click(function(event){
 	event.preventDefault();
+	// TODO: Disable form so you can't clikc multiple times and create multiple users
+	// TODO: Check if user already exists
 
 	$.post("create_user.php", $("#create_user_form").serialize(), function(data){
+	
+});
+
+});
+
+
+
+
+$("#login_button").click(function(event){
+	event.preventDefault();
+
+	$.post("login.php", $("#login_form").serialize(), function(data){
+		if (data.success){
+			window.location.href = 'logged_in.php';
+		}
+		
 	
 });
 
@@ -22,37 +38,6 @@ $("#create_user_button").click(function(event){
 
 
 
-
-
-
-
-function validateEmailInput(){
-
-}
-
-/*
-		$(function(){
-			$("#email").change(function(){
-				alert("hej");
-
-			});
-
-			$("#lekdiv").text(
-				"<?php echo $_POST['email']; ?>"
-
-				);
-			
-			
-		});
-
-		$("#button").onclick(() => alert("hejsan"));
-
-
-
-
-
-
-*/
 
 
 
