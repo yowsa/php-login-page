eb4d3175be7e5fa03fa53261da109c18d61b7989
yowsa <josefin@fundin.com>
2019-09-28 16:10:16 +0100

    updated logic for the post request and its responses

--- public_html/create_user.php
+++ public_html/create_user.php
@@ -8,20 +8,21 @@ if ($_POST){
 	$confim_password = $_POST["confirm_password"];
 	if ($accounts_table->checkIfEmailExists($email)){
 		json_responder(False, "This email is already associated with an existing account.");
-		exit();
-	}
-	if (!$accounts_table->validateEmail($email)){
+
+	} else if (!$accounts_table->validateEmail($email)){
 		json_responder(False, "Please enter a valid email address.");
-		exit();
-	}
-	if ($password == $confim_password) {
+
+	} else if ($password != $confim_password) {
+		json_responder(False, "Your passwords didn't match. Please try again");
+
+	} else {
 		$accounts_table->addUser($email, $password);
 		json_responder(True, "User created. You are being redirected to the login page.");
-	} else {
-		json_responder(False, "Your passwords didn't match. Please try again");
 	}
 
-exit();
+	// TODO: Should ideally be in a transaction
+
+	exit();
 }
 
 
@@ -34,13 +35,13 @@ require_once "../resources/templates/header.php"
 <div> 
 	<form id="create_user_form" method="post"> 
 		Email: <input type="name" name="email"><p>
-		Password: <input type="password" name="password"><p>
-		Confirm Password: <input type="password" name="confirm_password"><p>
-		<input type="submit" id="create_user_button" value="Create User">
-	</form>
-</div>
-<div id="create_user_message"></div>
-<a href="login.php">Login</a>
+			Password: <input type="password" name="password"><p>
+				Confirm Password: <input type="password" name="confirm_password"><p>
+					<input type="submit" id="create_user_button" value="Create User">
+				</form>
+			</div>
+			<div id="create_user_message"></div>
+			<a href="login.php">Go to Login</a>
 
 
 
