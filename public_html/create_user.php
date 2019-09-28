6a350de23758500540f3898dc4bcaf4ef85d0db9
yowsa <josefin@fundin.com>
2019-09-28 14:35:55 +0100

    removed some dead code

--- public_html/create_user.php
+++ public_html/create_user.php
@@ -2,28 +2,13 @@
 require_once "../base.php";
 require_once "../db_connection.php";
 
-// TODO: check so that the passwords provided are the same
 if ($_POST){
 	if ($_POST["password"] == $_POST["confirm_password"]) {
-		//$secure_password = $password_security->createSecurePassword($_POST["password"]);
 		$accounts_table->addUser($_POST["email"], $_POST["password"]);
-		//TODO: Create json response function that creates an array and takes a message
 		json_responder(True, "User created. You are being redirected to the login page.");
-
-		//$user_creation_sussessful = [
-		//	"success" => True
-		//];
-		//header('Content-Type: application/json');
-		//echo json_encode($user_creation_sussessful);
-		
 	} else {
 		json_responder(False, "Your passwords didn't match. Please try again");
-
-
-
 	}
-
-
 
 exit();
 }
@@ -31,10 +16,6 @@ exit();
 
 require_once "../resources/templates/header.php" 
 ?>
-
-
-
-
 
 
 
