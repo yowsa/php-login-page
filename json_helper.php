bf1e9cebad2d36517a9834b6b2f37862d9c0ea6c
yowsa <josefin@fundin.com>
2019-09-28 15:44:04 +0100

    fixed some formatting

--- json_helper.php
+++ json_helper.php
@@ -1,17 +1,13 @@
 <?php
-function json_responder($bool, $response_message){
+function json_responder($bool, $response_message=""){
 	$response = [
-			"success" => $bool,
-			"message" => $response_message,
-		];
+		"success" => $bool,
+		"message" => $response_message,
+	];
 
 	header('Content-Type: application/json');
 	echo json_encode($response);
 
 }
-
-
-
-
 
 ?>
