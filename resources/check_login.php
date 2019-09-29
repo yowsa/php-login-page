429af44c019e482d6c55032a3c999c259d6a6860
yowsa <josefin@fundin.com>
2019-09-29 11:42:06 +0100

    removed dead code

--- resources/check_login.php
+++ resources/check_login.php
@@ -3,12 +3,4 @@ if (!$_SESSION['user_validated']){
 	header("Location: login.php");
 	
 } 
-
-
-//if ($require_login AND $_SESSION['user_validated']){
-//	header("Location: http://www.josefin.fundin.com");
-	
-//} 
-
-
 ?>
